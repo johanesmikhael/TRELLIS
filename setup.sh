b26bb0cf158351e55bde5764c0e009ca64cedb7c
Mikhael Johanes <johanes.mikhael@gmail.com>
2025-01-27 17:56:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -73,7 +73,7 @@ fi
 # Get system information
 WORKDIR=$(pwd)
 PYTORCH_VERSION=$(python -c "import torch; print(torch.__version__)")
-PLATFORM=$(python -c "import torch; print(('cuda' if torch.version.cuda else ('hip' if torch.version.hip else 'unknown')) if torch.cuda.is_available() else 'cpu')")
+PLATFORM=$(python -c "import torch; print(('cuda' if torch.version.cuda else ('hip' if torch.version.hip else 'unknown')))")
 case $PLATFORM in
     cuda)
         CUDA_VERSION=$(python -c "import torch; print(torch.version.cuda)")
